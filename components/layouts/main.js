5b3417c0740eb967bafa1fb836393309ab586002
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-04-22 10:19:00 +0900

    Create Main

---
+++ components/layouts/main.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import { Box, Container } from "@chakra-ui/react";
+import { checkIsManualRevalidate } from "next/dist/server/api-utils";
+
+const Main = ({ children, router }) => {
+    return (
+        <Box as="main" pb={8}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>K Hhoshi - Homepage</title>
+            </Head>
+
+            <Container maxW="container.md" pt={14}>
+                {children}
+            </Container>
+        </Box>
+    )
+}
+
+export default Main
