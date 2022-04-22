f563bef02954f2c53f701e0a5af2995145f458d9
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-04-22 10:34:13 +0900

    簡単な紹介ページ完成

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
     return (
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I&apos;m a developer based in Japan!
+            </Box>
+
+            <Box display={{ md: "flex" }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        K Hhoshi
+                    </Heading>
+                    <p>Digital Craftzman ( Developer )</p>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
