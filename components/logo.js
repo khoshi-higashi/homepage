a123187b962bb6dd0850aafe24fbc29fc331e513
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-04-23 13:27:29 +0900

    スペース幅変更

--- components/logo.js
+++ components/logo.js
@@ -18,20 +18,20 @@ padding: 10px;
 `
 
 const Logo = () => {
-    const footPrintImg = "/images/footprint${useColorModeValue('', '-dark')}.png"
+  const footPrintImg = "/images/footprint${useColorModeValue('', '-dark')}.png"
 
-    return (
-        <Link href="/">
-            <a>
-                <LogoBox>
-                    <Image src={footPrintImg} width={20} height={20} alt="logo" />
-                    <Text color={useColorModeValue("gray.800", "whiteAlpha.900")} fontFamily="M PLUS Rounded 1c" fontWeight="bold" ml={3} >
-                        K Hoshi
-                    </Text>
-                </LogoBox>
-            </a>
-        </Link>
-    )
+  return (
+    <Link href="/">
+      <a>
+        <LogoBox>
+          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Text color={useColorModeValue("gray.800", "whiteAlpha.900")} fontFamily="M PLUS Rounded 1c" fontWeight="bold" ml={3} >
+            K Hoshi
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  )
 }
 
 export default Logo
