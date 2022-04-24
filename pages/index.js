02ad3e339a11dc1baf0090271a7871a65c375739
hoshimaro <89444685+khoshi-higashi@users.noreply.github.com>
2022-04-24 22:01:23 +0900

    プロフィール画像を追加

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, Image, useColorModeValue } from "@chakra-ui/react"
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box borderRadius="lg" bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")} p={3} mb={6} align="center">
         Hello, I&apos;m a developer based in Japan!
       </Box>
 
@@ -15,7 +15,7 @@ const Page = () => {
           <p>Digital Craftzman ( Developer )</p>
         </Box>
         <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align="center">
-          <Image />
+          <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth="100px" display="inline-block" borderRadius="full" src="https://cdn.hinatazaka46.com/files/14/diary/official/member/moblog/202204/mobl53w0c.jpg" alt="Profile Image" />
         </Box>
       </Box>
     </Container>
